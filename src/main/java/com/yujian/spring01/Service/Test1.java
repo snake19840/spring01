0893cc273f5a9f6e7f5e816680db47b9232c522d
yujian19840 <snake19840@aliyun.com>
2020-09-20 08:20:37 +0800

    Initial commit

---
+++ src/main/java/com/yujian/spring01/Service/Test1.java
@@ -0,0 +1,10 @@
+package com.yujian.spring01.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Test1 {
+  public  int Add(int a,int b){
+        return a+b;
+    }
+}
